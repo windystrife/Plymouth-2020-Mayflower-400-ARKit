afcaabb481d9e6ea6062b0c952da456a212924b4
Tung Thanh Nguyen <tungnt.rec@gmail.com>
2018-03-06 23:29:19 +0700

    Ver 1.2
    Ver 1.2

---
+++ Source/ARPortalVideo/ARPortalVideoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARPortalVideoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPORTALVIDEO_API AARPortalVideoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
